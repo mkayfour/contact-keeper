2c4c9fb4bc4f98f283a3e3531912ad6e13b1873e
Mohit Kumar <mk4227525@gmail.com>
2020-03-11 14:33:48 +0530

    contact context

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navabar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import ContactState from "./context/contact/ContactState";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navabar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navabar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 }
 
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,70 @@
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import ContactReducer from "./contactReducer";
+
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        type: "personal",
+        id: 1,
+        name: "Mohit Kumar",
+        email: "mk4227525@gmail.com",
+        phone: "8975956469"
+      },
+      {
+        type: "professional",
+        id: 2,
+        name: "Ted Mosby",
+        email: "tedmosby@gmail.com",
+        phone: "8975956469"
+      },
+      {
+        type: "professional",
+        id: 2,
+        name: "Sarah Watson",
+        email: "sarahwatson@gmail.com",
+        phone: "8975956469"
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initalState);
+
+  // Add contact
+
+  // Delete contact
+
+  // Set current contact
+
+  // Clear current contact
+
+  // Update contact
+
+  // Filter contact
+
+  // Clear current
+
+  return (
+    <ContactContext.Provider>
+      value=
+      {{
+        contacts: state.contacts
+      }}
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
